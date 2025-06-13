8e3f283b5dd67608b21c81d89ba94723c6092c58
manish <bandamanishkumar7@gmail.com>
2025-06-13 16:01:13 +0530

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react"
 import LandingPage from "./pages/landingpage/Landingpage";
-
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./pages/homepage/Homepage"
 function App(){
   return (
-    <LandingPage/>
+  
+    <Routes>
     
+      <Route path=""  element={<LandingPage/>}/>
+      <Route path="/home" element={<Homepage></Homepage>}/>
+    </Routes>
+  
   )
 };
 
